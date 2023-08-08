fc304400d3a85ce2defb774436ec365a6f111f65
sanfrancecil <141710534+sanfrancecil@users.noreply.github.com>
2023-08-07 23:55:15 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit
-import pandas as pd
+
 
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
@@ -9,6 +9,7 @@ streamlit.text('Hard-Boiled Free-Range Egg')
 
 streamlit.text('🥣 🥗 🐔 🥑🍞')
 
+import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.dataframe(my_fruit_list)
 
