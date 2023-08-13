e9b556aca2bf5982d61e582b3ea1ea74913cee05
sanfrancecil <141710534+sanfrancecil@users.noreply.github.com>
2023-08-12 21:56:32 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,6 +42,7 @@ streamlit.dataframe(fruityvice_normalized)
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
 my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
 
 my_data_row = my_cur.fetchone()
